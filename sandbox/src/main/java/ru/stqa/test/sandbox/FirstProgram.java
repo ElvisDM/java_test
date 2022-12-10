1c92b1c86391737940615bd11c77f296bd389a47
ElvisDM <sv_dm@list.ru>
2022-12-10 16:52:47 +0300

    Update FirstProgram.java

--- sandbox/src/main/java/ru/stqa/test/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/test/sandbox/FirstProgram.java
@@ -3,7 +3,7 @@ package ru.stqa.test.sandbox;
 public class FirstProgram {
 
   public static void main(String[] args) {
-   System.out.println("Hello, world!");
+    System.out.println("Hello, world!");
   }
 
 }
