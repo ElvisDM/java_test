cf251dfc29ca9a789aa79e72d918b82a49c75925
ElvisDM <sv_dm@list.ru>
2023-01-05 14:27:29 +0300

    Лекции 4.5, 4.6 и 4.7.
    Лекция 4.5. Формирование коллекции.
    Лекция 4.6. Сравнение коллекций.
    Лекция 4.7. Множества: неупорядоченные коллекции.

--- addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/model/GroupData.java
@@ -3,14 +3,31 @@ package ru.stqa.test.addressbook.model;
 import java.util.Objects;
 
 public final class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
   }
 
   public String name() {
@@ -26,26 +43,24 @@ public final class GroupData {
   }
 
   @Override
-  public boolean equals(Object obj) {
-    if (obj == this) return true;
-    if (obj == null || obj.getClass() != this.getClass()) return false;
-    var that = (GroupData) obj;
-    return Objects.equals(this.name, that.name) &&
-            Objects.equals(this.header, that.header) &&
-            Objects.equals(this.footer, that.footer);
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name, header, footer);
+    return Objects.hash(id, name);
   }
 
   @Override
   public String toString() {
-    return "GroupData[" +
-            "name=" + name + ", " +
-            "header=" + header + ", " +
-            "footer=" + footer + ']';
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.test.addressbook.model.GroupData;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
   @Test
@@ -12,14 +15,18 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test4", null, null));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test4", null, null);
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
 
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
