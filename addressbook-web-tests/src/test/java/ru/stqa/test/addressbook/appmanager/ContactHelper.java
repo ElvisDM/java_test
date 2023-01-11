197481bdd72b76b4d6284586caa5bcfe11932a65
ElvisDM <sv_dm@list.ru>
2023-01-11 20:35:28 +0300

    Лекция 5.10.
     Режем строки (и немного про регулярные выражения)

--- addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/appmanager/ContactHelper.java
@@ -101,8 +101,10 @@ public class ContactHelper extends HelperBase {
       List<WebElement> cells = element.findElements(By.tagName("td"));
       String lastname = cells.get(1).getText();
       String firstname = cells.get(2).getText();
+      String[] phones = cells.get(5).getText().split("\n");
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
+      contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname)
+              .withHomephone(phones[0]).withMobilephone(phones[1]).withWorkphone(phones[2]));
     }
     return contacts;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/tests/ContactPhoneTests.java
@@ -1,31 +1,26 @@
 package ru.stqa.test.addressbook.tests;
 
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.test.addressbook.model.ContactData;
-import ru.stqa.test.addressbook.model.GroupData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTests extends TestBase{
-  @BeforeMethod
-  public void ensurePreconditions(){
-    app.goTo().groupPage();
-    String group = "test1";
-    if (! app.contact().isThereACheckGroupName(group)) {
-      app.group().create(new GroupData().withName(group).withHeader("test2").withFooter("test3"));
-    }
-    app.goTo().gotoHome();
-    if (app.contact().all().size() == 0) {
-      app.contact().create(new ContactData().withFirstname("Viktor").withLastname("Brovin").withAddress("Russia").
-              withHomephone("+7(901)683-09-76").withMobilephone("+7 (914) 923-53-82").withWorkphone("+7 (922) 984-23-43")
-              .withMail("brovin19@mail.ru").withGroup(group));
-    }
-  }
+
   @Test
   public void testContactPhones() {
     app.goTo().gotoHome();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
+    assertThat(contact.getHomephone(), equalTo(cleaned(contactInfoFromEditForm.getHomephone())));
+    assertThat(contact.getMobilephone(), equalTo(cleaned(contactInfoFromEditForm.getMobilephone())));
+    assertThat(contact.getWorkphone(), equalTo(cleaned(contactInfoFromEditForm.getWorkphone())));
+
+  }
+  public String cleaned(String phone) {
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
 
 }
