c93b5c6cf763e468eb8ebe50b8f1fccf132610ea
ElvisDM <sv_dm@list.ru>
2023-02-19 23:05:05 +0300

    Лекция 10.4.
    Генерация отчётов при помощи Allure.

--- addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/test/addressbook/tests/TestBase.java
@@ -3,10 +3,8 @@ package ru.stqa.test.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.stqa.test.addressbook.appmanager.ApplicationManager;
 import ru.stqa.test.addressbook.model.ContactData;
 import ru.stqa.test.addressbook.model.Contacts;
@@ -20,6 +18,8 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Listeners(MyTestListener.class)
+
 public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
@@ -27,8 +27,9 @@ public class TestBase {
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void setUp(ITestContext context) throws Exception {
     app.init();
+    context.setAttribute("app", app);
   }
 
   @AfterSuite(alwaysRun = true)
